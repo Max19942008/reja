b9c6a3106be80fadc64db7cea1d1801cb659788b
Rakhimboev Bobur <boburrahimboyev2008@gmail.com>
2025-06-03 00:28:33 +0900

    feat:develop author router js page

--- server.js
+++ server.js
@@ -3,6 +3,14 @@ console.log("web serverni boshlash");
 const express = require("express");
 const app = express();
 const http = require("http");
+const fs = require("fs");
+
+let user;
+fs.readFile("database/user.json","utf8", (err, data) =>{
+if(err) {
+  console.log("ERROR:", err);
+}
+});
 //1 Kirish kodlari
 app.use(express.static("public"));
 app.use(express.json());
@@ -22,6 +30,9 @@ console.log(req.body);
 res.json({test: "success"});
 });
 
+app.get('/author',(req , res) => {
+res.render("author", {user: user});
+});
 
 app.get("/", function (req ,res) {
   res.render("harid");
